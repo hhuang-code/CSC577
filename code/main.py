2efe762a9f9d19d92015124e57717f051f7da594
huanghao-code <huanghao_bupt@sina.com>
2017-11-30 09:21:01 -0500

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,7 +1,19 @@
 from tools.data_loader import load_data
+import numpy as np
+import pdb
 
-data_dir = '../data/'
-test_data_name = 'SumMe'
-model_type = 2
+if __name__ == '__main__':
 
-load_data(data_dir, test_data_name, model_type)
+    # load data
+    data_dir = '../data/'
+    test_data_name = 'SumMe'
+    model_type = 2  # 1 for vsLSTM, 2 for dppLSTM
+    print('...loading data')
+    train_set, valid_set, test_set, test_idx = load_data(data_dir, test_data_name, model_type)
+
+    items = []
+    for i in range(len(train_set[0])):
+        items.append([train_set[j][i] for j in range(len(train_set))])
+
+    pdb.set_trace()
+    print(len(items))    
