a7971a152b8f63bf1a0b7163151a30be1059e0a5
huanghao-code <huanghao_bupt@sina.com>
2017-12-02 11:02:04 -0500

    generate pool5 feature of ResNet

--- my_test.py
+++ my_test.py
@@ -5,6 +5,9 @@ from torchvision import transforms, models
 import torch.nn as nn
 from torch.autograd import Variable
 
+import h5py
+import numpy as np
+
 import pdb
 
 """
@@ -63,7 +66,7 @@ class ResNet(nn.Module):
                 ])
 
         if fea_type == 'resnet101':
-            resnet = models.resnet101(pretrained = True)
+            resnet = models.resnet101(pretrained = True)    # dim of pool5 is 2048
         elif fea_type == 'resnet152':
             resnet = models.resnet152(pretrained = True)
         else:
@@ -81,7 +84,7 @@ class ResNet(nn.Module):
     def forward(self, x):
         x = self.transform(x)
         x = x.unsqueeze(0)  # reshape the single image s.t. it has a batch dim
-        pdb.set_trace()
+        #pdb.set_trace()
         x = Variable(x).cuda()
         res_conv5 = self.conv5(x)
         res_pool5 = self.pool5(res_conv5)
@@ -92,15 +95,25 @@ class ResNet(nn.Module):
 
 if __name__ == '__main__':
     
-    dir_path = Path('/home/aaron/Documents/Courses/577/dataset/frame/Youtube/v11')
-    
+    dir_path = Path('/home/aaron/Documents/Courses/577/dataset/frame/Youtube/v20')
+
     resnet = ResNet(224)
 
-    images = []
-    for image_path in sorted(dir_path.glob('*.jpg')):   # for each image in dir_path
-        image = default_loader(str(image_path))
+    video_feature = np.array([])
+    #pdb.set_trace()
+    for frame_path in sorted(dir_path.glob('*.jpg')):   # for each frame image in dir_path
+        frame = default_loader(str(frame_path))
+        print(frame_path)
+        # extract ResNet feature
+        res_conv5, res_pool5 = resnet(frame)
+        # gpu variable -> cpu variable -> tensor -> numpy array -> 1D array
+        frame_feature = res_pool5.cpu().data.numpy().flatten()
+        if video_feature.size == 0:
+            video_feature = np.hstack((video_feature, frame_feature))
+        else:
+            video_feature = np.vstack((video_feature, frame_feature))
+        print(video_feature.shape)
 
-        res_conv5, res_pool5 = resnet(image)
-
-        print(res_pool5)
-
+    h5file = h5py.File('/home/aaron/Documents/Courses/577/dataset/feature/Youtube/v20.h5', 'w')
+    h5file.create_dataset('pool5', data = video_feature)
+    h5file.close()
